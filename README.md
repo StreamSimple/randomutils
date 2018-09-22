4c0aadfe7038eae9b14ea01b1c5c2b909da643a7
Timothy Farkas <tim@streamsimple.io>
2018-09-21 21:37:02 -0700

    - Updated pom to use correct config.  - Added license headers.  - Added readme with build badge.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Java Utilities Project [![Build Status](https://travis-ci.org/StreamSimple/randomutils.svg?branch=master)](https://travis-ci.org/StreamSimple/randomutils)
+
+This project provides simple probability utilities.
--- src/test/java/com/streamsimple/randomutils/UniformListSamplerTest.java
+++ src/test/java/com/streamsimple/randomutils/UniformListSamplerTest.java
@@ -1,24 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.streamsimple.randomutils;
 
-import org.junit.Assert;
-import org.junit.Test;
 import java.util.HashSet;
 import java.util.Set;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class UniformListSamplerTest
 {
   @Test
-  public void stressTest() {
+  public void stressTest()
+  {
     stressTestMultiHelper(100, 50, 10);
   }
 
-  private void stressTestMultiHelper(int testCount, int listSize, int sampleSize) {
+  private void stressTestMultiHelper(int testCount, int listSize, int sampleSize)
+  {
     for (int counter = 0; counter < testCount; counter++) {
       stressTestHelper(listSize, sampleSize);
     }
   }
 
-  private void stressTestHelper(int listSize, int sampleSize) {
+  private void stressTestHelper(int listSize, int sampleSize)
+  {
     final UniformListSampler listSampler = new UniformListSampler();
     final int[] sampleIndices = listSampler.sample(listSize, sampleSize);
 
